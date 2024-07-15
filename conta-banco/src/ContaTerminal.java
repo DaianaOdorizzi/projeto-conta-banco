68a58115cb7880a078f72e21930431f8ccc2e053
DaianaOdorizzi <d.lealodorizzi@gmail.com>
2024-07-15 17:17:09 -0300

    meu projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        
+        String nomeCliente = "Daiana Odorizzi";
+        double saldoConta = 237.48;
+
+        System.out.println("Por favor, digite o o número da sua conta!");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite o o número da sua agencia!");
+        Double agenciaConta = scanner.nextDouble();
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaConta +", conta " + numeroConta +" e seu saldo " + saldoConta +" já está disponível para saque");
+        scanner.close();
+
+        
     }
 }
